37b894a60b05a23e7a7d99cc8d7a025a08228db5
Chernov2312 <113832662+Chernov2312@users.noreply.github.com>
2026-04-22 13:20:58 +0700

    рабочая админка + добавлен валидатор

---
+++ person_ville/users/validators.py
@@ -0,0 +1,13 @@
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+class RoleValidate:
+    ALLOWED_ROLES = ['admin', 'player']
+
+    def __call__(self, value):
+        if value not in self.ALLOWED_ROLES:
+            raise ValidationError(
+                _('Роль может быть только admin или player'),
+                params={'value': value},
+            )

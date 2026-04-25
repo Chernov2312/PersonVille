18468d6fabb3934735bdc3dddffab340d10bc091
Chernov2312 <113832662+Chernov2312@users.noreply.github.com>
2026-04-25 19:39:26 +0700

    расположение констант поправлено

--- person_ville/quizzes/forms.py
+++ person_ville/quizzes/forms.py
@@ -1,17 +1,17 @@
 __all__ = ('EntryAnswerForm',)
 from django import forms
 
-REQUIRED_ERROR_MESSAGE = {'required': 'Выбери один вариант ответа.'}
-ENTRY_ANSWER_CHOICES = [
-    (1, 'Совсем не похоже'),
-    (2, 'Скорее не похоже'),
-    (3, 'И да, и нет'),
-    (4, 'Скорее похоже'),
-    (5, 'Очень похоже'),
-]
-
 
 class EntryAnswerForm(forms.Form):
+    REQUIRED_ERROR_MESSAGE = {'required': 'Выбери один вариант ответа.'}
+    ENTRY_ANSWER_CHOICES = [
+        (1, 'Совсем не похоже'),
+        (2, 'Скорее не похоже'),
+        (3, 'И да, и нет'),
+        (4, 'Скорее похоже'),
+        (5, 'Очень похоже'),
+    ]
+
     answer = forms.ChoiceField(
         choices=ENTRY_ANSWER_CHOICES,
         widget=forms.RadioSelect,
